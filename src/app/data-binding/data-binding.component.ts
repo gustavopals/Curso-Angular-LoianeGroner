d2f79eeb48b898e929f2197fd746aab2eb96cf93
Gustavo Pals <gustavoandrepals@gmail.com>
2020-09-23 11:21:30 -0300

    Testes Gerais

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -2,110 +2,110 @@ import { CursosService } from './../cursos/cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-data-binding',
-    templateUrl: './data-binding.component.html',
-    styleUrls: ['./data-binding.component.css']
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent implements OnInit {
+  url = 'https://www.google.com.br';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+  nome = 'abc';
+  nomeDoCurso = 'Angular Data-Binding';
+  valorAtual = '';
+  valorSalvo: string;
+  valorSalvo2: string;
+  isMouseOver = false;
+  resultadoFinal = 0;
+  num1 = '0';
+  num2 = '0';
+  valor = 5;
+  deletarCiclo = false;
+  testeIf = false;
+  cursosCadastrados: string[] = ['Java', 'Angular', 'PHP', 'Android'];
+  meuFavorito = false;
+  cursos: string[] = [];
+  teste: boolean;
 
-    url = 'https://www.google.com.br';
-    cursoAngular = true;
-    urlImagem = 'http://lorempixel.com/400/200/nature/';
-    nome = 'abc';
-    nomeDoCurso: string = 'Angular Data-Binding';
-    valorAtual: string = '';
-    valorSalvo: string;
-    valorSalvo2: string;
-    isMouseOver: boolean = false;
-    resultadoFinal: number = 0;
-    num1: string = '0';
-    num2: string = '0';
-    valor: number = 5;
-    deletarCiclo: boolean = false;
-    testeIf = false;
-    cursosCadastrados: string[] = ["Java", "Angular", "PHP", "Android"];
-    meuFavorito: boolean = false;
-    cursos: string[] = [];
-    teste: boolean;
+  ativo = false;
+  tamanhoFonte = 10;
 
-    ativo: boolean = false;
-    tamanhoFonte: number = 10;
+  pessoa: any = {
+    nome: 'Gustavo',
+    idade: 24,
+    telefone: {
+      DDD: 42,
+      numero: 999365350,
+    },
+  };
 
-    pessoa: any = {
-        nome: 'Gustavo',
-        idade: 24,
-        telefone: {
-            DDD: 42,
-            numero: 999365350
-        }
-    }
+  mudarAtivo() {
+    this.ativo = !this.ativo;
+  }
 
-    mudarAtivo() {
-        this.ativo = !this.ativo;
-    }
+  onClick() {
+    this.meuFavorito = !this.meuFavorito;
+  }
 
-    onClick() {
-        this.meuFavorito = !this.meuFavorito;
-    }
+  // tslint:disable-next-line: typedef
+  onTesteIf() {
+    // altera o valor de testeIf
+    this.testeIf = !this.testeIf;
+  }
+  destruirCiclo() {
+    this.deletarCiclo = true;
+  }
 
-    onTesteIf() { //altera o valor de testeIf
-        this.testeIf = !this.testeIf;
-    }
-    destruirCiclo() {
-        this.deletarCiclo = true;
-    }
+  mudarValor() {
+    this.valor++;
+  }
 
-    mudarValor() {
-        this.valor++;
-    }
+  fazCalculo() {
+    return 321231;
+  }
 
-    fazCalculo() {
-        return 321231;
-    }
+  getValor() {
+    return 1;
+  }
 
-    getValor() {
-        return 1;
-    }
+  getCurtirCurso() {
+    return true;
+  }
 
-    getCurtirCurso() {
-        return true;
-    }
+  botaoClicado() {
+    alert('Botão clicado');
+  }
 
-    botaoClicado() {
-        alert('Botão clicado');
-    }
+  onKeyUp(evento: KeyboardEvent) {
+    console.log(evento); // Captura todos os eventos executados no campo
+    console.log((evento.target as HTMLInputElement).value);
+    this.valorAtual = (evento.target as HTMLInputElement).value;
+  }
 
-    onKeyUp(evento: KeyboardEvent) {
-        console.log(evento); //Captura todos os eventos executados no campo
-        console.log((<HTMLInputElement>evento.target).value);
-        this.valorAtual = (<HTMLInputElement>evento.target).value;
-    }
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
 
-    salvarValor(valor) {
-        this.valorSalvo = valor;
-    }
+  salvarValor2(valor) {
+    this.valorSalvo2 = valor;
+  }
 
-    salvarValor2(valor) {
-        this.valorSalvo2 = valor;
-    }
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
 
-    onMouseOverOut() {
-        this.isMouseOver = !this.isMouseOver;
-    }
+  constructor(private cursosService: CursosService) {}
 
-    constructor(private cursosService: CursosService) { }
+  ngOnInit(): void {
+    this.cursos = this.cursosService.getCursos();
+  }
 
-    ngOnInit(): void {
-        this.cursos = this.cursosService.getCursos();
-    }
+  soma() {
+    return parseInt(this.valorSalvo) + parseInt(this.valorSalvo2);
+  }
 
-    soma() {
-        return parseInt(this.valorSalvo) + parseInt(this.valorSalvo2);
-    }
-
-    somaFinal() {
-        this.resultadoFinal = parseInt(this.num1) + parseInt(this.num2);
-        console.log("somou");
-    }
-
+  somaFinal() {
+    this.resultadoFinal = parseInt(this.num1) + parseInt(this.num2);
+    console.log('somou');
+  }
 }
--- src/app/http-curso/http-curso.service.ts
+++ src/app/http-curso/http-curso.service.ts
@@ -5,47 +5,44 @@ import { Injectable } from '@angular/core';
 import { Produto } from '../models/produto';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpCursoService {
+  private API = `${environment.API}Produto`;
 
-    private API = `${environment.API}Produto`;
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
+  list() {
+    return this.http
+      .get<Produto[]>(this.API)
+      .pipe(delay(100), tap(console.log));
+  }
 
-    list() {
-        return this.http.get<Produto[]>(this.API)
-            .pipe(
-                delay(100),
-                tap(console.log)
-            );
+  create(curso) {
+    alert('Produto cadastrado com sucesso!');
+    return this.http.post(this.API, curso); //Utiliza do metodo Post, pega a Url da Api + o objeto passado pelo componente 'curso'
+  }
+
+  update(curso) {
+    alert('Produto alterado com sucesso!');
+    return this.http.put(this.API + '/' + curso.id, curso);
+  }
+
+  delete(id) {
+    console.log('chegou no service', id);
+
+    return this.http.delete(this.API + '/' + id);
+  }
+
+  loadId(id) {
+    return this.http.get(this.API + '/' + id);
+  }
+
+  save(curso) {
+    if (curso.id) {
+      return this.update(curso);
+    } else {
+      return this.create(curso);
     }
-
-    create(curso) {
-        alert("Produto cadastrado com sucesso!");
-        return this.http.post(this.API, curso) //Utiliza do metodo Post, pega a Url da Api + o objeto passado pelo componente 'curso'
-    }
-
-    update(curso) {
-        alert("Produto alterado com sucesso!");
-        return this.http.put(this.API + '/' + curso.id, curso)
-    }
-
-    delete(id) {
-        console.log('chegou no service', id);
-
-        return this.http.delete(this.API + '/' + id);
-    }
-
-    loadId(id) {
-        return this.http.get(this.API + '/' + id);
-    }
-
-    save(curso) {
-        if (curso.id) {
-            return this.update(curso)
-        } else {
-            return this.create(curso)
-        }
-    }
+  }
 }
